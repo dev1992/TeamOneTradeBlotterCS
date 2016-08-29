ec1302e45d48e7657c97440defb4bad5670582f2
AnshumanKumar123 <anshuman053@gmail.com>
2016-08-29 12:40:08 +0530

    added one comment

--- TradeBlotterAppl/LoginWindow.xaml.cs
+++ TradeBlotterAppl/LoginWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace TradeBlotterAppl
     /// <summary>
     /// Interaction logic for LoginWindow.xaml
     /// </summary>
+    /// 
+    /// This uis a method
     public partial class LoginWindow : Window
     {
         public LoginWindow()
