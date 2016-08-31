8313c151d9a10b09a3190d2c45ce113944d61ce4
AnshumanKumar123 <anshuman053@gmail.com>
2016-08-31 18:45:18 +0530

    pagination added

--- TradeBlotterAppl/LoginWindow.xaml.cs
+++ TradeBlotterAppl/LoginWindow.xaml.cs
@@ -28,47 +28,55 @@ namespace TradeBlotterAppl
             InitializeComponent();
         }
 
-        private void LoginAction(object sender, RoutedEventArgs e)
+       
+
+        private void PageAction(object sender, RoutedEventArgs e)
         {
-            string username = txtUserName.Text;
-            string password = txtPassword.Password;
-            var client = new WebClient();
-            //string url = "http://10.87.226.147:8080/TeamOneTradeBlotterWeb/rest/traders/signin?username=" + userName + "&password=" + password;
-            //string response = user.DownloadString(url);
-
-            //if (response=="true")
-            //{
-            //    MainWindow userLog = new MainWindow();
-            //    bool? result1 = userLog.ShowDialog();
-            //}
-            //else
-            //{
-            //    MessageBox.Show(response);
-            //}
-            string naam;
-            using (client)
-            {
-                var values = new NameValueCollection();
-                values["username"] = username;
-                values["password"] = password;
-                var res = client.UploadValues("http://10.87.239.26:8080/TeamOneTradeBlotterFinalWeb/rest/traders/signinsecure", values);
-                var str = Encoding.Default.GetString(res);
-                if(str=="true")
-                {
-                    MainWindow userLog = new MainWindow();
-                    //FilterWindow filterWindowFilter = new FilterWindow();
-                     naam=txtUserName.Text;
-                    userLog.lstUserName.Items.Add(naam);
-                    bool? result1 = userLog.ShowDialog();
-                }
-                else
-                {
-                    MessageBox.Show("Sorry failed to log in. Try again!");
-                }
-
-            }
-
+            MainWindow userLog = new MainWindow();
+            bool? result1 = userLog.ShowDialog();
         }
+
+        //private void LoginAction(object sender, RoutedEventArgs e)
+        //{
+        //    string username = txtUserName.Text;
+        //    string password = txtPassword.Password;
+        //    var client = new WebClient();
+        //    //string url = "http://10.87.226.147:8080/TeamOneTradeBlotterWeb/rest/traders/signin?username=" + userName + "&password=" + password;
+        //    //string response = user.DownloadString(url);
+
+        //    //if (response=="true")
+        //    //{
+        //    //    MainWindow userLog = new MainWindow();
+        //    //    bool? result1 = userLog.ShowDialog();
+        //    //}
+        //    //else
+        //    //{
+        //    //    MessageBox.Show(response);
+        //    //}
+        //    string naam;
+        //    using (client)
+        //    {
+        //        var values = new NameValueCollection();
+        //        values["username"] = username;
+        //        values["password"] = password;
+        //        var res = client.UploadValues("http://10.87.239.26:8080/TeamOneTradeBlotterFinalWeb/rest/traders/signinsecure", values);
+        //        var str = Encoding.Default.GetString(res);
+        //        if(str=="true")
+        //        {
+        //            MainWindow userLog = new MainWindow();
+        //            //FilterWindow filterWindowFilter = new FilterWindow();
+        //             naam=txtUserName.Text;
+        //            userLog.lstUserName.Items.Add(naam);
+        //            bool? result1 = userLog.ShowDialog();
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("Sorry failed to log in. Try again!");
+        //        }
+
+        //    }
+
+        //}
 
         private void goToSignUp(object sender, RoutedEventArgs e)
         {
@@ -76,5 +84,5 @@ namespace TradeBlotterAppl
             bool? resultLoginAttempt = newUserAdd.ShowDialog();
         }
     }
-    }
+}
 
--- TradeBlotterAppl/MainWindow.xaml.cs
+++ TradeBlotterAppl/MainWindow.xaml.cs
@@ -17,6 +17,9 @@ using System.IO;
 using System.Runtime.Serialization.Json;
 using System.Runtime.Serialization;
 
+using System.Collections;
+using System.ComponentModel;
+
 namespace TradeBlotterAppl
 {
     /// <summary>
@@ -25,56 +28,188 @@ namespace TradeBlotterAppl
     public partial class MainWindow : Window
     {
         string urlFilter;
+        private readonly PagingCollectionView _cview;
+
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void ReadAPI(object sender, RoutedEventArgs e)
-        {
+            // -------------------------
+
             WebClient webClient = new WebClient();
             Stream data = webClient.OpenRead("http://10.87.239.26:8080/TeamOneTradeBlotterFinalWeb/rest/trades");
             //Stream data = webClient.OpenRead("http://10.87.239.26:8080/TeamOneTradeBlotterFinalWeb/rest/trades/filterbytype?productType=FX");
             DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(TradeData[]));
             TradeData[] other = (TradeData[])serializer.ReadObject(data);
-            string msg = "";
 
-            ////foreach (TradeData acc in other)
-            ////{
-            ////    msg += acc.ToString() +System.Environment.NewLine;
 
-            ////}
-            dataTrade.ItemsSource = other;
+            // ---------------------------
+
+
+            this._cview = new PagingCollectionView(
+                other,
+                25
+            );
+            this.DataContext = this._cview;
         }
 
-        private void filterTradeBlotter(object sender, RoutedEventArgs e)
+        private void OnNextClicked(object sender, RoutedEventArgs e)
         {
-            FilterWindow filterWindow = new FilterWindow();
-            //naam = txtUserName.Text;
-            //userLog.lstUserName.Items.Add(naam);
-            bool? result1 = filterWindow.ShowDialog();
-            if (result1==true)
-            {
-                MessageBox.Show(filterWindow.urlReturn().ToString());
-                WebClient webClient = new WebClient();
-                Stream data = webClient.OpenRead(filterWindow.urlReturn().ToString());
-                //Stream data = webClient.OpenRead("http://10.87.239.26:8080/TeamOneTradeBlotterFinalWeb/rest/trades/filterbytype?productType=fx");
-                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(TradeData[]));
-                TradeData[] other = (TradeData[])serializer.ReadObject(data);
-                string msg = "";
-                foreach (TradeData acc in other)
-                {
-                    msg += acc.ToString() +  System.Environment.NewLine;
+            this._cview.MoveToNextPage();
+        }
 
-                }
-                dataTrade.Items.Refresh();
-                dataTrade.ItemsSource = other;
-            }
-            else
-            {
+        private void OnPreviousClicked(object sender, RoutedEventArgs e)
+        {
+            this._cview.MoveToPreviousPage();
+        }
 
-            }
+        //private void ReadAPI(object sender, RoutedEventArgs e)
+        //{
+        //    WebClient webClient = new WebClient();
+        //    Stream data = webClient.OpenRead("http://10.87.239.26:8080/TeamOneTradeBlotterFinalWeb/rest/trades");
+        //    //Stream data = webClient.OpenRead("http://10.87.239.26:8080/TeamOneTradeBlotterFinalWeb/rest/trades/filterbytype?productType=FX");
+        //    DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(TradeData[]));
+        //    TradeData[] other = (TradeData[])serializer.ReadObject(data);
+        //    string msg = "";
+
+        //    ////foreach (TradeData acc in other)
+        //    ////{
+        //    ////    msg += acc.ToString() +System.Environment.NewLine;
+
+        //    ////}
+        //    dataTrade.ItemsSource = other;
+        //}
+
+        //private void filterTradeBlotter(object sender, RoutedEventArgs e)
+        //{
+        //    FilterWindow filterWindow = new FilterWindow();
+        //    //naam = txtUserName.Text;
+        //    //userLog.lstUserName.Items.Add(naam);
+        //    bool? result1 = filterWindow.ShowDialog();
+        //    if (result1==true)
+        //    {
+        //        MessageBox.Show(filterWindow.urlReturn().ToString());
+        //        WebClient webClient = new WebClient();
+        //        Stream data = webClient.OpenRead(filterWindow.urlReturn().ToString());
+        //        //Stream data = webClient.OpenRead("http://10.87.239.26:8080/TeamOneTradeBlotterFinalWeb/rest/trades/filterbytype?productType=fx");
+        //        DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(TradeData[]));
+        //        TradeData[] other = (TradeData[])serializer.ReadObject(data);
+        //        string msg = "";
+        //        foreach (TradeData acc in other)
+        //        {
+        //            msg += acc.ToString() +  System.Environment.NewLine;
+
+        //        }
+        //        dataTrade.Items.Refresh();
+        //        dataTrade.ItemsSource = other;
+        //    }
+        //    else
+        //    {
+
+        //    }
             
+        //}
+
+    }
+
+    // =============== Page Class
+
+    public class PagingCollectionView : CollectionView
+    {
+        private readonly IList _innerList;
+        private readonly int _itemsPerPage;
+
+        private int _currentPage = 1;
+
+        public PagingCollectionView(IList innerList, int itemsPerPage)
+            : base(innerList)
+        {
+            this._innerList = innerList;
+            this._itemsPerPage = itemsPerPage;
+        }
+
+        public override int Count
+        {
+            get
+            {
+                if (this._innerList.Count == 0) return 0;
+                if (this._currentPage < this.PageCount) // page 1..n-1
+                {
+                    return this._itemsPerPage;
+                }
+                else // page n
+                {
+                    var itemsLeft = this._innerList.Count % this._itemsPerPage;
+                    if (0 == itemsLeft)
+                    {
+                        return this._itemsPerPage; // exactly itemsPerPage left
+                    }
+                    else
+                    {
+                        // return the remaining items
+                        return itemsLeft;
+                    }
+                }
+            }
+        }
+
+        public int CurrentPage
+        {
+            get { return this._currentPage; }
+            set
+            {
+                this._currentPage = value;
+                this.OnPropertyChanged(new PropertyChangedEventArgs("CurrentPage"));
+            }
+        }
+
+        public int ItemsPerPage { get { return this._itemsPerPage; } }
+
+        public int PageCount
+        {
+            get
+            {
+                return (this._innerList.Count + this._itemsPerPage - 1)
+                    / this._itemsPerPage;
+            }
+        }
+
+        private int EndIndex
+        {
+            get
+            {
+                var end = this._currentPage * this._itemsPerPage - 1;
+                return (end > this._innerList.Count) ? this._innerList.Count : end;
+            }
+        }
+
+        private int StartIndex
+        {
+            get { return (this._currentPage - 1) * this._itemsPerPage; }
+        }
+
+        public override object GetItemAt(int index)
+        {
+            var offset = index % (this._itemsPerPage);
+            return this._innerList[this.StartIndex + offset];
+        }
+
+        public void MoveToNextPage()
+        {
+            if (this._currentPage < this.PageCount)
+            {
+                this.CurrentPage += 1;
+            }
+            this.Refresh();
+        }
+
+        public void MoveToPreviousPage()
+        {
+            if (this._currentPage > 1)
+            {
+                this.CurrentPage -= 1;
+            }
+            this.Refresh();
         }
     }
 }
